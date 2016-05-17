0ac529be5eb1d4579342c8047466a4c224c71603
wes <wesley.deglise@gmail.com>
2016-05-17 14:49:12 +0200

    add departments schema, add canonical_url to generic schema

--- service-types/couchapp/lib/schemas/generic.js
+++ service-types/couchapp/lib/schemas/generic.js
@@ -22,6 +22,18 @@ exports.v1 = {
         "producer_content_id": {
             "type": "string"
         },
+        "canonical_url": {
+            "type": "object",
+            "patternProperties": {
+                "^([a-z]{2}|und)$": {
+                    "type": "array",
+                    "items": {
+                        "type": "string",
+                        "pattern": "(http|https):\\\/\\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\\/|\\\/([\\w#!:.?+=&%@!\\-\\\/]))?"
+                    }
+                }
+            }
+        },
         "created": {
             "type": "string",
             "pattern": "^([0-9]{2,4})-([0-1][0-9])-([0-3][0-9])(?:( [0-2][0-9]):([0-5][0-9]):([0-5][0-9]))$"
@@ -65,6 +77,7 @@ exports.v1 = {
         "version",
         "producer",
         "producer_content_id",
+        "canonical_url",
         "created",
         "updated",
         "default_language",
