aaa7cf1f142e969aedd93ba1529e2ee629508bcb
wesley deglise <wesley.deglise@gmail.com>
2016-07-07 17:14:19 +0200

    feat(types): add news schema (#8)
    * feat(types): add news schema
    
    * feat(types): fix file extension
    
    * publish_date|update_date are standard fields
    
    * ISO8601 as per https://tools.ietf.org/html/rfc3339

---
+++ services/types/couchapp/lib/schemas/news.js
@@ -0,0 +1,68 @@
+{
+    "id": "news",
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "version": "1.0",
+    "type": "object",
+    "properties": {
+        "fields": {
+            "type": "object",
+            "patternProperties": {
+                "featured_image|featured_media|sources": {
+                    "type": "object",
+                    "patternProperties": {
+                        "^([a-z]{2}|und)$": {
+                            "type": "array",
+                            "items": {
+                                "type": "string",
+                                "pattern": "(http|https):\\\/\\\/(\\w+:{0,1}\\w*@)?(\\S+)(:[0-9]+)?(\\\/|\\\/([\\w#!:.?+=&%@!\\-\\\/]))?"
+                            }
+                        }
+                    }
+                },
+                "title": {
+                    "type": "object",
+                    "patternProperties": {
+                        "^([a-z]{2}|und)$": {
+                            "type": "array",
+                            "items": {
+                                "type": "string",
+                                "pattern": "[0-9A-Za-z-?! ]{1,256}$"
+                            }
+                        }
+                    },
+                    "additionalProperties": false
+                },
+                "long_title|description|news_type|introduction|body|location|background_information": {
+                    "type": "object",
+                    "patternProperties": {
+                        "^([a-z]{2}|und)$": {
+                            "type": "array"
+                        }
+                    },
+                    "additionalProperties": false
+                },
+                "publish_date|update_date": {
+					          "type": "string",
+                    "format":"date-time"
+                },
+                "department|person": {
+                    "type": "array",
+                    "items": {
+                        "type": "string",
+                        "pattern": "[0-9A-Za-z-?! ]{32}$"
+                    }
+                }
+            },
+            "required": [
+                "title",
+                "long_title",
+                "description",
+                "news_type",
+                "publish_date",
+                "update_date",
+                "body",
+                "location"
+            ]
+        }
+    }
+}
