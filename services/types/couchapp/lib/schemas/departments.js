a75a45ed87c4eabb2da7a1711e617202c59a8073
Yannick Huard <huard.yan@gmail.com>
2016-09-26 14:50:00 +0200

    fix(cs): update deps and fix last errors

--- services/types/couchapp/lib/schemas/departments.js
+++ services/types/couchapp/lib/schemas/departments.js
@@ -1,48 +1,48 @@
 exports.v1 = {
-  "id": "departments",
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "version": "1.0",
-  "description": "Departments document structure",
-  "definitions": {
-    "field_open" : {
-        "type": "object",
-        "patternProperties": {
-            "^([a-z]{2}|und)$": {
-                "type": "array"
-            }
-        },
-        "additionalProperties": false
+  id: 'departments',
+  $schema: 'http://json-schema.org/draft-04/schema#',
+  version: '1.0',
+  description: 'Departments document structure',
+  definitions: {
+    field_open: {
+      type: 'object',
+      patternProperties: {
+        '^([a-z]{2}|und)$': {
+          type: 'array'
+        }
+      },
+      additionalProperties: false
     }
   },
-  "type": "object",
-  "properties": {
-    "fields": {
-      "type": "object",
-      "patternProperties": {
-        "title": {
-          "type": "object",
-          "patternProperties": {
-            "^([a-z]{2}|und)$": {
-              "type": "array",
-              "items": {
-                "type": "string",
-                "pattern": "[0-9A-Za-z-?! ]{1,256}$"
+  type: 'object',
+  properties: {
+    fields: {
+      type: 'object',
+      patternProperties: {
+        title: {
+          type: 'object',
+          patternProperties: {
+            '^([a-z]{2}|und)$': {
+              type: 'array',
+              items: {
+                type: 'string',
+                pattern: '[0-9A-Za-z-?! ]{1,256}$'
               }
             }
           },
           additionalProperties: false
         },
-        "description": { "$ref": "departments#/definitions/field_open" },
-        "introduction": { "$ref": "departments#/definitions/field_open" },
-        "abbreviation": { "$ref": "departments#/definitions/field_open" },
-        "department_type": {
-          "type": "object",
-          "patternProperties": {
-            "^([a-z]{2}|und)$": {
-              "type": "array",
-              "items": {
-                "type": "string",
-                "enum": ["Directorate-General", "Service department", "Executive agency", "External department"]
+        description: { $ref: 'departments#/definitions/field_open' },
+        introduction: { $ref: 'departments#/definitions/field_open' },
+        abbreviation: { $ref: 'departments#/definitions/field_open' },
+        department_type: {
+          type: 'object',
+          patternProperties: {
+            '^([a-z]{2}|und)$': {
+              type: 'array',
+              items: {
+                type: 'string',
+                enum: ['Directorate-General', 'Service department', 'Executive agency', 'External department']
               }
             }
           },
