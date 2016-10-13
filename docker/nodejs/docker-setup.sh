d6c14b36742b25e5ccc0eb404690fa73d75fd409
Yannick Huard <mail@yannickhuard.com>
2016-10-13 09:03:59 +0200

    feat(changes): provide POC for (near) real-time changes notification (#23)
    * feat(changes): provide POC for (near) real-time changes notification
    
    * feat(changes): make use of environment variables to configure the app
    
    * feat(changes): update readme
    
    * chore(gitignore): remove ignored file
    
    * chore(gitignore): update gitignore
    
    * style(cs): fix coding standards
    
    * fix(changes): fix error and fine-tune docker compose
    
    * docs(setup): refactor documentation

--- docker/nodejs/docker-setup.sh
+++ docker/nodejs/docker-setup.sh
@@ -4,12 +4,12 @@
 npm run prepare && npm run build
 
 # Wait for CouchDB to be started
-dockerize -wait http://couchdb:5984 -timeout 10s
+dockerize -wait ${HOST} -timeout 10s
 
 # Create system databases
-curl -X PUT http://admin:pass@couchdb:5984/_users
-curl -X PUT http://admin:pass@couchdb:5984/_replicator
-curl -X PUT http://admin:pass@couchdb:5984/_global_changes
+curl -X PUT http://${USERNAME}:${PASSWORD}@${DOMAIN}:${PORT}/_users
+curl -X PUT http://${USERNAME}:${PASSWORD}@${DOMAIN}:${PORT}/_replicator
+curl -X PUT http://${USERNAME}:${PASSWORD}@${DOMAIN}:${PORT}/_global_changes
 
 # Push schemas
 npm run push
