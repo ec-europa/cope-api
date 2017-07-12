f20fe0530e2be1ce06c846db8eaa6975f59abd20
degliwe <wesley.deglise@gmail.com>
2017-07-12 15:19:42 +0200

    feat(service-changes)

--- services/changes/index.js
+++ services/changes/index.js
@@ -11,30 +11,20 @@ var changes = new ChangesStream({
   db: facade + '/beta/changes'
 });
 
-// Trigger action on change
-changes.on('readable', function handleChange() {
-  // Read changes
-  var change = changes.read();
-  var postData;
-
-  // Extract data (or do whatever you want)
-  console.info('Changes captured:');
-  console.info(change);
-
-  // Filter subscribers
+function getSubscribers(type, producer, theme) {
   request({
     url: facade + '/beta/subscriptions/subscribers/',
     method: 'POST',
     json: {
       selector: {
         type: {
-          $or: ['news', { $exists: false }]
+          $or: [type, { $exists: false }]
         },
         producer: {
-          $or: ['prd', { $exists: false }]
+          $or: [producer, { $exists: false }]
         },
         theme: {
-          $or: ['energy', { $exists: false }]
+          $or: [theme, { $exists: false }]
         },
         endpoint: { $exists: true }
       },
@@ -47,8 +37,44 @@ changes.on('readable', function handleChange() {
 
     if (!error) {
       console.log('Subscribers ' + response.statusCode + ' : ' + JSON.stringify(body));
+      // TODO for each entry ping the subscriber
     }
   });
+}
+
+function pingSubscriber(seq, endpoint, subscriptionUrl) {
+  var postData = {
+    seq: seq,
+    subscriptionUrl: subscriptionUrl
+  };
+
+  request({
+    url: endpoint,
+    method: 'POST',
+    json: postData
+  }, function handleResponse(error, response, body) {
+    if (error) {
+      console.error(error);
+    }
+    if (!error) {
+      console.log('Subscriber responded with ' + response.statusCode + ' : ' + body);
+    }
+  });
+}
+
+// Trigger action on change
+changes.on('readable', function handleChange() {
+  // Read changes
+  var change = changes.read();
+
+  // Extract data (or do whatever you want)
+  console.info('Changes captured:');
+  console.info(change);
+
+  // TODO fetch changed document and extract 'type','producer','theme'
+
+  // Filter subscribers
+  getSubscribers('news', 'prd', 'energy');
 
   // Mock subscriber's endpoint
   nock('http://listener.com')
@@ -56,23 +82,6 @@ changes.on('readable', function handleChange() {
     .reply(202, 'SUCCESS');
 
   // Webhook
-  postData = {
-    seq: change.seq
-  };
-
   console.info('Sending a ping to subscriber');
-
-  request({
-    url: 'http://listener.com/endpoint',
-    method: 'POST',
-    json: postData
-  }, function handleResponse(error, response, body) {
-    if (error) {
-      console.error(error);
-    }
-
-    if (!error) {
-      console.log('Subscriber responded with ' + response.statusCode + ' : ' + body);
-    }
-  });
+  pingSubscriber(change.seq, 'http://listener.com/endpoint', 'subscription url');
 });
