eb84aca2a6e7f9de9a52860129a2eb6946c5b7c5
degliwe <wesley.deglise@gmail.com>
2017-07-13 09:19:35 +0200

    feat(service-changes): check type of changes, get the list of subscriber and ping them

--- services/changes/index.js
+++ services/changes/index.js
@@ -11,34 +11,77 @@ var changes = new ChangesStream({
   db: facade + '/beta/changes'
 });
 
-function getSubscribers(type, producer, theme) {
-  request({
-    url: facade + '/beta/subscriptions/subscribers/',
-    method: 'POST',
-    json: {
+function getChangedDocument(change) {
+  console.log('Get changed document :', change.id);
+  return new Promise(function (success, failure) {
+    request({
+      url: facade + '/beta/docs/' + change.id,
+      method: 'GET'
+    }, function handleResponse(error, response, body) {
+      if (error) {
+        console.error('problem!!! ', error);
+        failure(error);
+      }
+
+      if (!error) {
+        body = JSON.parse(body);
+        console.log('Extract changed type : ', body.type, body.producer, body.theme);
+        success({
+          seq: change.seq,
+          type: body.type,
+          producer: body.producer,
+          theme: body.theme
+        });
+      }
+    });
+  });
+}
+
+function getSubscribers(params) {
+  return new Promise(function (success, failure) {
+    var req = {
       selector: {
-        type: {
-          $or: [type, { $exists: false }]
-        },
-        producer: {
-          $or: [producer, { $exists: false }]
-        },
-        theme: {
-          $or: [theme, { $exists: false }]
-        },
         endpoint: { $exists: true }
       },
-      fields: ['theme', 'type', 'producer', '_id', 'consumer', 'endpoint']
-    }
-  }, function handleResponse(error, response, body) {
-    if (error) {
-      console.error(error);
+      fields: ['_id', 'consumer', 'endpoint']
+    };
+
+    if (params.type) {
+      req.selector.type = {
+        $or: [params.type, { $exists: false }]
+      };
     }
 
-    if (!error) {
-      console.log('Subscribers ' + response.statusCode + ' : ' + JSON.stringify(body));
-      // TODO for each entry ping the subscriber
+    if (params.producer) {
+      req.selector.producer = {
+        $or: [params.producer, { $exists: false }]
+      };
     }
+
+    if (params.theme) {
+      req.selector.theme = {
+        $or: [params.theme, { $exists: false }]
+      };
+    }
+
+    request({
+      url: facade + '/beta/subscriptions/subscribers/',
+      method: 'POST',
+      json: req
+    }, function handleResponse(error, response, body) {
+      if (error) {
+        console.error('problem!!! ', error);
+        failure(error);
+      }
+
+      if (!error) {
+        console.log('Get subscribers list : ', JSON.stringify(body.docs));
+        success({
+          docs: body.docs,
+          seq: params.seq
+        });
+      }
+    });
   });
 }
 
@@ -62,26 +105,29 @@ function pingSubscriber(seq, endpoint, subscriptionUrl) {
   });
 }
 
+function pingSubscribers(params) {
+  params.docs.forEach(function (doc) {
+    // Mock subscriber's endpoint
+    nock('http://listener.com')
+      .post('/endpoint')
+      .reply(202, 'SUCCESS');
+
+    // Webhook
+    console.info('Ping subscriber : ', doc.consumer);
+    pingSubscriber(params.seq, 'http://listener.com/endpoint', doc._id);
+  });
+}
+
 // Trigger action on change
 changes.on('readable', function handleChange() {
   // Read changes
   var change = changes.read();
 
   // Extract data (or do whatever you want)
-  console.info('Changes captured:');
-  console.info(change);
+  console.info('Changes captured:', change.id);
 
   // TODO fetch changed document and extract 'type','producer','theme'
-
-  // Filter subscribers
-  getSubscribers('news', 'prd', 'energy');
-
-  // Mock subscriber's endpoint
-  nock('http://listener.com')
-    .post('/endpoint')
-    .reply(202, 'SUCCESS');
-
-  // Webhook
-  console.info('Sending a ping to subscriber');
-  pingSubscriber(change.seq, 'http://listener.com/endpoint', 'subscription url');
+  getChangedDocument(change)
+    .then(getSubscribers)
+    .then(pingSubscribers);
 });
